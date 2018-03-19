4e7850ebfee1504ee1061ae724686d21270ab671
Nashira Emery <nashiraemery1@gmail.com>
2018-03-18 23:50:25 -0400

    ruby OOP practice problem

--- ruby.rb
+++ ruby.rb
@@ -49,14 +49,40 @@
 
 
 
-Return the largest number from the array without using index lookups.
-[[1,2,3],[4,5,6]]:
+# Return the largest number from the array without using index lookups.
+# [[1,2,3],[4,5,6]]:
+#
+# [[1,2,3],[4,5,6]].max.max
+#
+#
+# Write Ruby to calculate the following:
+# 1. Find the maximum number in the first array.
+# 2. Find the minimum number in the last array. :
+#
+# [[1,2,3],[4,5,6]][1].min > [0].max
 
-[[1,2,3],[4,5,6]].max.max
 
 
-Write Ruby to calculate the following:
-1. Find the maximum number in the first array.
-2. Find the minimum number in the last array. :
 
-[[1,2,3],[4,5,6]][1].min > [0].max
+# Create a class variable named @@everyoneand set it equal to an empty array. In this array we are going to store all the instances of Person, try to do this by creating a save method. The save method should also print @@everyone out to the console, so we can see all the people that are in our storage.
+
+
+class Person
+
+  @@everyone = []
+  attr_accessor :name, :hair_color
+
+  def initialize(name, hair_color)
+    @name = name
+    @hair_color = hair_color
+  end
+
+  def sing
+    puts " #{@name} is singing "
+  end
+
+  def save
+    @@everyone << self
+    puts @@everyone
+  end
+end
